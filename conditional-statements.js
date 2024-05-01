a229a6b913ed92ae9a8a6b1e0bc964881c8166c7
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-05-01 18:42:36 +0800

    added a conditional statement tutorial video both .html and .js file

---
+++ conditional-statements.js
@@ -0,0 +1,54 @@
+var num = 25
+var EnterNumber = Number(prompt("Enter a valid number? "))
+
+if(num <= EnterNumber){
+    console.log("That's a good number")
+}
+
+var hour = 7
+var Time = Number(prompt("What time did you wake up? "))
+
+if(hour > Time || Time == 7){
+    console.log("Good job! you woke up early this time")
+}else{
+    console.log("You woke up late! you better hurry up! and get dressed already!")
+}
+
+var grade = 90;
+var total = Number(prompt("Enter your total grade: "))
+
+if(grade <= total){
+    console.log("Good job! You got an A-grade! Keep it up!")
+}else if(total >= 80){
+    console.log("Good job! You got a B-grade!")
+}else if(total >= 70){
+    console.log("Please make some more efforts on your grade, but you did a pretty good job! You got a C-grade")
+}else{
+    console.log("Your in the failing grade! You better study harder!")
+}
+
+var day = Number(prompt("What day is it? "))
+var dayname;
+
+switch(day){
+    case 1:
+        dayname = "Monday"
+        console.log("It's Monday!")
+        break;
+    case 2:
+        dayname = "Tuesday"
+        console.log("It's Tuesday!")
+        break;
+    case 3:
+        dayname = "Wednesday"
+        console.log("It's Wednesday!")
+        break;
+    case 4:
+        dayname = "Thursday"
+        console.log("It's Thursday!")
+        break;
+    case 5:
+        dayname = "Friday"
+        console.log("It's Friday!")
+        break;
+}
